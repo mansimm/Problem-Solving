7352e48ec6b036686c8cd34d22583e66ebd08df3
Mansi <43803851+mansimm@users.noreply.github.com>
2021-08-29 10:07:58 +0530

    solved

---
+++ LeetCode - Easy/Numbers/Two Sum/Solution.java
@@ -0,0 +1,27 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+*/
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        int ans[] = new int[2];
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=i+1;j<nums.length;j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    ans[0]=i;
+                    ans[1]=j;
+                    return ans;
+                }
+            }
+        }
+        return ans;
+    }
+}
