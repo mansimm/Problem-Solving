760f2395bd33987c93c445136bad85a67fefe5e5
Mansi <43803851+mansimm@users.noreply.github.com>
2025-05-15 21:43:11 +0530

    Create Solution2.java

---
+++ Leetcode/1D DP/70. Climbing Stairs/Solution2.java
@@ -0,0 +1,17 @@
+//1D DP
+class Solution {
+    public int climbStairs(int n) {
+        if(n==0 || n==1){
+            return 1;
+        }
+        int temp1 = 1;// 1 way to reach if n=1
+        int temp2 = 2;//2 ways to reach if n=2
+        for(int i=3;i<=n;i++){//3, 4
+            int current = temp1+temp2;//3, 5
+            temp1 = temp2;//2, 3
+            temp2 = current;//3, 5
+        }
+        return temp2;
+    }
+}
+//Time c O(n), space c O(1)
