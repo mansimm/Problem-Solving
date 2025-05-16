f3a959ee22faf7a9edceef3e448ae7f53eb71180
Mansi <43803851+mansimm@users.noreply.github.com>
2025-05-16 22:34:38 +0530

    Create Solution2.java

---
+++ Leetcode/Multidimensional DP/Unique Paths II/Solution2.java
@@ -0,0 +1,23 @@
+//1DP
+class Solution2 {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[] dp = new int[n];//to prevent integer overflow
+        dp[0] = 1;
+        //this solution process one row at a time
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(obstacleGrid[i][j]==1){
+                    dp[j]=0;
+                }else if(j>0){//to prevent null pointer exception
+                    dp[j] = dp[j] + dp[j-1];
+                    //dp[j] is unique path till j from above row (from up)
+                    //dp[j-1] is unique path from prev col(left)
+                }
+            }
+        }
+        return dp[n-1];
+    }
+}
+//Time complexity : O(m*n) and space : O(n)
