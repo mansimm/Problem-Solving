efd491e6ad7fabd62dd70240163f2af2e0aa7417
Mansi <43803851+mansimm@users.noreply.github.com>
2025-06-04 21:31:20 +0530

    Create Solution1.java

---
+++ Leetcode/Array_String/151. Reverse Words in a String/Solution1.java
@@ -0,0 +1,22 @@
+/*
+Steps:
+1.	Trim starting and ending space using trim()
+2.	Then convert string to array of string by splitting using spaces
+3.	Then revers array elements by two pointer method
+4.	Then join all array elements and return result
+  */
+class Solution1 {
+    public String reverseWords(String s) {
+        String[] arr = s.trim().split("\\s+");
+        //System.out.println(Arrays.toString(arr)); // print the array before reversing
+        //System.out.println(arr.length);
+        for(int i=0,j=arr.length-1;i<j;i++,j--){
+            System.out.println(arr[i] + " " + arr[j]  );
+            String temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+        return String.join(" ",arr);
+    }
+}
+//Note: Both time and space complexities of above solution is O(n).
