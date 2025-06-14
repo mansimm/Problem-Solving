18dde8d33e6130331d6b5014fd17b6cc34360080
Mansi <43803851+mansimm@users.noreply.github.com>
2025-06-14 17:28:52 +0530

    290. Word Pattern

--- Leetcode/HashMap/290. Word Pattern/Solution.java
+++ Leetcode/HashMap/290. Word Pattern/Solution.java
@@ -1,4 +1,16 @@
-	class Solution {
+/*
+Steps:
+1.	Split s by “ “ into array of string
+2.	We will first check if size of arr and pattern are same or not, if not same return false
+3.	Then create a map to store char mapping from arr words  to pattern
+4.	We will iterate over arr and pattern using i var
+5.	Fetch string and chars at i from arr and pattern in a and b respectively
+6.	Check if map contains key a, if yes we will check if its value is b, if not return false
+7.	If map does not contains key a and value b then we will add (a,b) pair in map
+8.	If map contains value b, return false. Bcoz no 2 chars can map to same value string
+
+*/
+class Solution {
 	    public boolean wordPattern(String pattern, String s) {
 	        String[] arr = s.split(" ");
 	        if(arr.length!=pattern.length()){
