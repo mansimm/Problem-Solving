95f24805d10a99af796a6d8648276f0c0f43d9a9
Mansi <43803851+mansimm@users.noreply.github.com>
2025-06-26 20:17:06 +0530

    141. Linked List Cycle

---
+++ Leetcode/Linked List/141. Linked List Cycle/Solution1.java
@@ -0,0 +1,30 @@
+//Hint: use slow and fast pointers
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head==null || head.next==null){
+            return false;
+        }
+        ListNode temp1 = head;
+        ListNode temp2 = temp1.next;
+        while(temp1!=null && temp2!=null && temp2.next!=null){
+            if(temp1==temp2){
+                return true;
+            }
+            temp1 = temp1.next;
+            temp2 = temp2.next.next;
+        }
+        return false;
+    }
+}
+//Time c = O(n) , space c = O(1)
