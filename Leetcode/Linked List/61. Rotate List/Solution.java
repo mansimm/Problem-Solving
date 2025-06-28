e3fe2bed25833e206b50ff6fc191afa28c90ead3
Mansi <43803851+mansimm@users.noreply.github.com>
2025-06-28 15:10:21 +0530

    61. Rotate List

---
+++ Leetcode/Linked List/61. Rotate List/Solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(k==0||head==null){
+            return head;
+        }
+        int len = getLength(head);
+        k = k%len;
+        if(len==1||k==0){
+            return head;
+        }
+        ListNode current = head;
+        for(int i=1;i<len-k;i++){
+            current=current.next;
+        }
+        ListNode next = current.next;
+        current.next = null;
+        ListNode temp = next;
+        while(temp!=null && temp.next!=null){
+            temp=temp.next;
+        }
+        temp.next=head;
+        head=next;
+        return head;
+    }
+
+    public int getLength(ListNode head){
+        int count = 0;
+        for(ListNode temp=head; temp!=null; temp=temp.next){
+            count++;
+        }
+        return count;
+    }
+}
+//Time c O(n) , space O(1)
